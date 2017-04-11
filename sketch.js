9f6a50e728204bdcc6b76456e40ba00c82bcdaa1
TaNaiyah Faison <tfaison@wlapcs.org>
2017-04-11 17:47:59 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var x = 300;
+var y = 200;
+var newHeight;
+
+
+
+
+function setup(){
+    createCanvas(600,400);
+    background(179, 66, 244);
+    x = 50;
+    y = 50;
+    newHeight = x + y;
+    
+    
+    
+}
+function draw(){
+    TaNaiyah();
+    noStroke();
+    fill(60, 220, 240);
+    ellipse(mouseX,mouseY, 100, newHeight);
+}
+
+function TaNaiyah(){
+    if(mouseX > 300);
+    if(mouseY > 200);
+}
+
+    
